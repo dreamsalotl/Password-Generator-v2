162e87ce22a3b199e8c6cfda6d17d550b2b2bf90
dreamsalotl <donovanw.walker@gmail.com>
2024-08-14 06:56:57 -0500

    created generator function

--- src/generator.js
+++ src/generator.js
@@ -1,3 +1,23 @@
 export const generatePassword = (length, options) => {
     const { includeUppercase, includeLowercase, includeNumbers, includeSymbols } = options;
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let allChars = '';
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeLowercase) allChars += lowercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+
+    if (allChars === '') return '';
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+    
+    return password;
 }
